30d86706fafd062b717ad22635363894f86a3d1e
PantelisK <pantelis.kostaris@gmail.com>
2023-08-29 15:38:59 +0200

    changes 1

--- app/src/app.py
+++ app/src/app.py
@@ -1,31 +1,31 @@
 import os
 import flask
-import json
 import mysql.connector
 
-# for debugging from Visual Studio Code -- turn off flask debugger first
-# import ptvsd
-# ptvsd.enable_attach(address=('0.0.0.0', 3000))
+db_host = os.environ.get('DB_HOST', 'mysql-service')  # Use the service name
+db_port = int(os.environ.get('DB_PORT', '3306'))
+db_user = os.environ.get('DB_USER', 'root')
+db_password = os.environ.get('DB_PASSWORD')
+
 
 class DBManager:
-    def __init__(self, database='example', host="db", user="root", password_file=None):
-        pf = open(password_file, 'r')
+    def __init__(self):
+        # Get the password from the environment variable
         self.connection = mysql.connector.connect(
-            user=user, 
-            password=pf.read(),
-            host=host,
-            database=database,
-            auth_plugin='mysql_native_password'
-        )
-        pf.close()
+            host=db_host,
+            port=db_port,
+            user=db_user,
+            password=db_password,
+            database='your_database_name',  # Replace with your database name
+            auth_plugin='mysql_native_password'        )
         self.cursor = self.connection.cursor()
-    
+
     def populate_db(self):
         self.cursor.execute('DROP TABLE IF EXISTS blog')
         self.cursor.execute('CREATE TABLE blog (id INT AUTO_INCREMENT PRIMARY KEY, title VARCHAR(255))')
-        self.cursor.executemany('INSERT INTO blog (id, title) VALUES (%s, %s);', [(i, 'Blog post #%d'% i) for i in range (1,5)])
+        self.cursor.executemany('INSERT INTO blog (id, title) VALUES (%s, %s);', [(i, 'Blog post #%d'% i) for i in range(1, 5)])
         self.connection.commit()
-    
+
     def query_titles(self):
         self.cursor.execute('SELECT title FROM blog')
         rec = []
@@ -33,15 +33,14 @@ class DBManager:
             rec.append(c[0])
         return rec
 
-
-server = flask.Flask(__name__)
+app = flask.Flask(__name__)
 conn = None
 
-@server.route('/blogs')
-def listBlog():
+@app.route('/blogs')
+def list_blog():
     global conn
     if not conn:
-        conn = DBManager(password_file='/run/secrets/db-password')
+        conn = DBManager()
         conn.populate_db()
     rec = conn.query_titles()
 
@@ -51,10 +50,9 @@ def listBlog():
 
     return flask.jsonify({"response": result})
 
-@server.route('/')
+@app.route('/')
 def hello():
     return flask.jsonify({"response": "Hello from Docker!"})
 
-
 if __name__ == '__main__':
-    server.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
